d8a7eb8d32e067e7c011af90f5376931da4d2f31
fishluv <110374653+fishluv@users.noreply.github.com>
2023-10-14 23:34:56 -0700

    more tweaks

--- components/Home.tsx
+++ components/Home.tsx
@@ -7,9 +7,36 @@ export default function Home() {
   return (
     <div className={styles.Home}>
       <p className={styles.header}>pop&apos;n tools</p>
-      <Link href="/randomizer">
-        <Card title="🎲&nbsp;&nbsp;Randomizer" color="red" />
-      </Link>
+
+      <div>
+        <Link href="/randomizer">
+          <Card title="🎲&nbsp;&nbsp;Randomizer" color="red" />
+        </Link>
+      </div>
+
+      <div>
+        <Link href="/songs">
+          <Card title="🔎&nbsp;&nbsp;Song search" color="yellow" />
+        </Link>
+      </div>
+
+      <div>
+        <Link href="/charts">
+          <Card title="🔎&nbsp;&nbsp;Chart search" color="yellow" />
+        </Link>
+      </div>
+
+      <div>
+        <Link href="/listmaker">
+          <Card title="📓&nbsp;&nbsp;List maker" color="green" />
+        </Link>
+      </div>
+
+      <div>
+        <Link href="/tiermaker">
+          <Card title="📶&nbsp;&nbsp;Tier maker" color="blue" />
+        </Link>
+      </div>
     </div>
   )
 }
