c494e826b0b35f9c7f660aff6afa8f0d77ac3aef
fishluv <110374653+fishluv@users.noreply.github.com>
2023-10-15 00:31:10 -0700

    search stub

--- components/SongSearchPage.tsx
+++ components/SongSearchPage.tsx
@@ -1,14 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./SongSearchPage.module.scss"
 
 export default function SongSearchPage() {
+  const [searchQuery, setSearchQuery] = useState("")
+  const [songSearchResults, setSongSearchResults] = useState([])
+
+  function onInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target
+    // Remove leading whitespace. Only one trailing space allowed.
+    const sanitized = value.replace(/^\s+/, "").replace(/\s+$/, " ")
+    if (sanitized !== searchQuery) {
+      setSearchQuery(value)
+      // call search api
+    }
+  }
+
   return (
     <div className={styles.SongSearchPage}>
       <input
         className={styles.input}
         type="text"
         placeholder="🎵 Search for songs"
+        value={searchQuery}
+        onChange={onInputChange}
       />
+      {songSearchResults.map((song, index) => (
+        <p key={index}>{song}</p>
+      ))}
     </div>
   )
 }
