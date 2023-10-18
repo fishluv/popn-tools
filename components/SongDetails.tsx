91aa601e8d520f47ba62d477abf6058367f7dbe8
fishluv <110374653+fishluv@users.noreply.github.com>
2023-10-18 00:15:46 -0700

    song detail styling and content

--- components/SongDetails.tsx
+++ components/SongDetails.tsx
@@ -1,3 +1,4 @@
+import cx from "classnames"
 import React from "react"
 import styles from "./SongDetails.module.scss"
 import FolderPill from "./FolderPill"
@@ -5,16 +6,65 @@ import Song from "../models/Song"
 import SongBanner from "./SongBanner"
 import SongLevelPills from "./SongLevelPills"
 
+function Detail({
+  className,
+  field,
+  value,
+}: {
+  className?: string
+  field: string
+  value: string
+}) {
+  return (
+    <div className={cx(className, styles.Detail)}>
+      <span className={styles.field}>{field}</span>
+      <span className={styles.value}>{value}</span>
+    </div>
+  )
+}
+
 export default function SongDetails({ song }: { song: Song }) {
+  const { title, genre, genreRomanTrans, remywikiTitle } = song
   return (
     <div className={styles.SongDetails}>
       <SongBanner className={styles.banner} songId={song.id} />
+
+      {title === genre ? (
+        <>
+          <Detail className={styles.title} field="title/genre" value={title} />
+          {title !== remywikiTitle && (
+            <Detail
+              className={styles.romanized}
+              field=""
+              value={remywikiTitle}
+            />
+          )}
+        </>
+      ) : (
+        <>
+          <Detail className={styles.title} field="title" value={title} />
+          {title !== remywikiTitle && (
+            <Detail
+              className={styles.romanized}
+              field=""
+              value={remywikiTitle}
+            />
+          )}
+          <Detail
+            className={styles.genre}
+            field="genre"
+            value={genreRomanTrans}
+          />
+        </>
+      )}
+
       <div className={styles.folder}>
         <FolderPill folder={song.folder} style="full" />
       </div>
       <div className={styles.levels}>
         <SongLevelPills song={song} style="compact" />
       </div>
+
       <a href={`https://remywiki.com/${song.remywikiUrlPath}`} target="_blank">
         {song.title}
       </a>
