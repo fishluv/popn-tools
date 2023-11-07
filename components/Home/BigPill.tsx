6b03a99c71fbfd0c2290c5b22016d19f2293941b
fishluv <110374653+fishluv@users.noreply.github.com>
2023-11-07 00:38:30 -0800

    refactor bigpill and add beautiful icons

--- components/Home/BigPill.tsx
+++ components/Home/BigPill.tsx
@@ -4,25 +4,36 @@ import cx from "classnames"
 import Link from "next/link"
 
 interface BigPillProps {
-  title: string
+  className?: string
   color: "red" | "yellow" | "green" | "blue" | "purple"
   href?: string
+  children: React.ReactNode
 }
 
-export default function BigPill({ title, color, href }: BigPillProps) {
+export default function BigPill({
+  className,
+  color,
+  href,
+  children,
+}: BigPillProps) {
   if (href) {
     return (
       <Link
         href={href}
-        className={cx(styles.BigPill, styles[color], styles.clickable)}
+        className={cx(
+          className,
+          styles.BigPill,
+          styles[color],
+          styles.clickable,
+        )}
       >
-        <p className={styles.title}>{title}</p>
+        {children}
       </Link>
     )
   } else {
     return (
-      <div className={cx(styles.BigPill, styles[color])}>
-        <p className={styles.title}>{title}</p>
+      <div className={cx(className, styles.BigPill, styles[color])}>
+        {children}
       </div>
     )
   }
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,31 +1,34 @@
 import React from "react"
 import styles from "./Home.module.scss"
 import BigPill from "./BigPill"
+import { CgDice3, CgSearch } from "react-icons/cg"
+import { TbLayoutList } from "react-icons/tb"
+import { LuBarChartHorizontal } from "react-icons/lu"
 
 export default function Home() {
   return (
     <div className={styles.Home}>
       <p className={styles.header}>pop&apos;n tools</p>
 
-      <BigPill
-        title="🎲&nbsp;&nbsp;Randomizer"
-        color="red"
-        href="/randomizer"
-      />
+      <BigPill className={styles.BigPill} color="red" href="/randomizer">
+        <CgDice3 size="1.25rem" strokeWidth={1} />
+        <span>Chart randomizer</span>
+      </BigPill>
 
-      <BigPill title="🎵&nbsp;&nbsp;Search" color="purple" href="/search" />
+      <BigPill className={styles.BigPill} color="purple" href="/search">
+        <CgSearch size="1.25rem" strokeWidth={1} />
+        <span>{"Song & chart search"}</span>
+      </BigPill>
 
-      <BigPill
-        title="📓&nbsp;&nbsp;List maker"
-        color="green"
-        href="/listmaker"
-      />
+      <BigPill className={styles.BigPill} color="green" href="/listmaker">
+        <TbLayoutList size="1.25rem" strokeWidth={3} />
+        <span>List maker</span>
+      </BigPill>
 
-      <BigPill
-        title="📶&nbsp;&nbsp;Tier maker"
-        color="blue"
-        href="/tiermaker"
-      />
+      <BigPill className={styles.BigPill} color="blue" href="/tiermaker">
+        <LuBarChartHorizontal size="1.25rem" strokeWidth={3} />
+        <span>Tier maker</span>
+      </BigPill>
     </div>
   )
 }
