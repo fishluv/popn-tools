a8f3aed00ad79e62e5939a4333fd3e61512ad29a
fishluv <110374653+fishluv@users.noreply.github.com>
2024-06-12 22:15:07 -0700

    add song page handler

--- pages/chart/[songSlug]/index.tsx
+++ pages/chart/[songSlug]/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router"
 import ChartPage from "../../../components/ChartPage"
+import Link from "next/link"
 
 export function makeChartPageHandler(difficulty: "e" | "n" | "h" | "ex") {
   return function () {
@@ -11,4 +12,33 @@ export function makeChartPageHandler(difficulty: "e" | "n" | "h" | "ex") {
     // Flatten string[] into string but preserve undefined.
     return ChartPage(songSlug ? String(songSlug) : songSlug, difficulty)
   }
+}
+
+export default function SongPageHandler() {
+  const {
+    query: { songSlug },
+    asPath,
+  } = useRouter()
+
+  return (
+    <div>
+      {songSlug && (
+        <>
+          <div>{String(songSlug).replaceAll("-", " ")}</div>
+          <div>
+            <Link href={`${asPath}/e`}>easy</Link>
+          </div>
+          <div>
+            <Link href={`${asPath}/n`}>normal</Link>
+          </div>
+          <div>
+            <Link href={`${asPath}/h`}>hyper</Link>
+          </div>
+          <div>
+            <Link href={`${asPath}/ex`}>ex</Link>
+          </div>
+        </>
+      )}
+    </div>
+  )
 }
