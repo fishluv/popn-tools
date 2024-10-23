3f5f90a3756859a91f8bdef962c1fcc8efc8a22f
fishluv <110374653+fishluv@users.noreply.github.com>
2024-10-22 18:14:19 -0700

    add credit (long overdue lol)

--- components/common/More.tsx
+++ components/common/More.tsx
@@ -1,6 +1,7 @@
 import cx from "classnames"
 import styles from "./More.module.scss"
 import useLocalStorage from "../../lib/useLocalStorage"
+import Link from "next/link"
 
 export default function More({
   className,
@@ -14,6 +15,19 @@ export default function More({
   return (
     <div className={cx(className, styles.More)}>
       {children}
+
+      <h6>Credit</h6>
+      <p>
+        Romanized titles and genres come from{" "}
+        <Link href="https://remywiki.com" target="_blank">
+          RemyWiki
+        </Link>
+        . Ratings and S乱 levels come from{" "}
+        <Link href="https://popn.wiki" target="_blank">
+          popn.wiki
+        </Link>
+        .
+      </p>
 
       <h6>Extra options</h6>
       <input
